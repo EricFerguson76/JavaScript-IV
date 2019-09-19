9683a8b390424312963be5359e04f5445ad0b039
EricFerguson76 <turtle0888@gmail.com>
2019-09-19 18:07:56 -0500

    Added class info

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -20,10 +20,9 @@ class Instructor extends Person {
     this.specialty = instructAttr.specialty;
     this.favLanguage = instructAttr.favLanguage;
     this.catchPhrase = instructAttr.catchPhrase;
-
   }
   demo() {
-    return `Today we are learning about ${this.subject}`
+    return `Today we are learning about ${this.subject}`;
   }
   grade() {
     return `${this.name} revieves a perfect score on ${this.subject}`;
@@ -37,22 +36,21 @@ class Student extends Person {
     super(studentAttr);
     this.previousBackground = studentAttr.previousBackground;
     this.className = studentAttr.className;
-  }
-  favSubjects() {
-
+    this.favSubjects = studentAttr.favSubjects;
   }
 
   listsSubjects() {
-
+    return `${this.favSubjects[0]}, ${this.favSubjects[1]}, ${this.favSubjects[2]}`;
   }
-
-  PRAssignment() {
-    return `{student.name} has submitted a PR for {subject}.`;
+  PRAssignment(student, subject) {
+    return `${student.name} has submitted a PR for      ${subject}.`;
   }
-  sprintChallenge() {
-    return `{student.name} has begun sprint challenge on {subject}.`;
+  sprintChallenge(student, subject) {
+    return `${student.name} has begun sprint challenge on ${subject}.`;
   }
 }
+
+
 
 // PROJECT MANAGER
 
@@ -62,10 +60,97 @@ class ProjectManager extends Instructor {
     this.gradeClassName = manageAttr.gradeClassName;
     this.favInstructor = manageAttr.favInstructor;
   }
-  standUp() {
-    return `{name} announces to {channel} @channel stadny times!`;
+  standUp(name, channel) {
+    return `${name} announces to ${channel} @channel stadny times!`;
   }
-  debugsCode() {
-    return `{name} debugs {student.name}'s code on {subject}`;
+  debugsCode(name, student) {
+    return `${name} debugs ${student.name}'s code on {subject}`;
   }
 }
+
+// PERSON INFO
+
+const Ginny = new Person({
+  name: 'Ginny',
+  age: 22,
+  location: 'Florida',
+});
+
+const Hermione = new Person({
+  name: 'Hermione',
+  age: 23,
+  location: 'Scotland',
+});
+
+
+// INSTRUCTOR INFO
+
+const Snape = new Instructor({
+  name: 'Snape',
+  age: 43,
+  location: 'Colorado',
+  specialty: 'Redux',
+  favLanguage: 'Python',
+  catchPhrase: 'See ya on the flip side'
+});
+
+const Minerva = new Instructor({
+  name: 'Minerva',
+  age: 48,
+  location: 'Californa',
+  specialty: 'SQL',
+  favLanguage: 'Elm',
+  catchPhrase: 'If you die, you need not hand it in.'
+});
+
+
+// STUDENT INFO
+const Harry = new Student({
+  name: 'Harry',
+  age: 23,
+  location: 'England',
+  previousBackground: 'lived with aunt and uncle',
+  className: 'cS120',
+  favSubjects: [
+    'HTML', 'CSS', 'JavaScript'
+  ],
+});
+
+const Ron = new Student({
+  name: 'Ron',
+  age: 23,
+  location: 'Canada',
+  previousBackground: 'lived with mum and dad',
+  className: 'JS120',
+  favSubjects: [
+    'React', 'Python', 'JavaScript'
+  ],
+});
+
+
+// PROJECT MANAGERS
+
+const Albus = new ProjectManager({
+  name: 'Albus',
+  age: 53,
+  location: 'Alaska',
+  specialty: 'Node',
+  favLanguage: 'Java',
+  catchPhrase: 'It does not do to dwell on dreams and forget to live.',
+  gradeClassName: 'DS3',
+  favInstructor: 'Hagrid',
+});
+
+const Flitwick = new ProjectManager({
+  name: 'Flitwick',
+  age: 51,
+  location: 'Costa Rica',
+  specialty: 'AWS',
+  favLanguage: 'React',
+  catchPhrase: 'Keep the shiny side up.',
+  gradeClassName: 'CS2',
+  favInstructor: 'Lupin',
+});
+
+
+// CONSOLE LOGS
