b05e7fa9ce15a6a858dc6709ed0952d36d5e57f7
EricFerguson76 <turtle0888@gmail.com>
2019-09-19 20:52:42 -0500

    Updates to lambda

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -21,11 +21,11 @@ class Instructor extends Person {
     this.favLanguage = instructAttr.favLanguage;
     this.catchPhrase = instructAttr.catchPhrase;
   }
-  demo() {
+  demo(subject) {
     return `Today we are learning about ${this.subject}`;
   }
-  grade() {
-    return `${this.name} revieves a perfect score on ${this.subject}`;
+  grade(name, subject) {
+    return `${this.name} reviews a perfect score on ${this.subject}`;
   }
 } // this closes instructor
 
@@ -43,7 +43,7 @@ class Student extends Person {
     return `${this.favSubjects[0]}, ${this.favSubjects[1]}, ${this.favSubjects[2]}`;
   }
   PRAssignment(student, subject) {
-    return `${student.name} has submitted a PR for      ${subject}.`;
+    return `${student.name} has submitted a PR for ${subject}.`;
   }
   sprintChallenge(student, subject) {
     return `${student.name} has begun sprint challenge on ${subject}.`;
@@ -63,17 +63,19 @@ class ProjectManager extends Instructor {
   standUp(name, channel) {
     return `${name} announces to ${channel} @channel stadny times!`;
   }
-  debugsCode(name, student) {
-    return `${name} debugs ${student.name}'s code on {subject}`;
+  debugsCode(name, student, subject) {
+    return `${name} debugs ${student.name}'s code on ${subject}`;
   }
 }
+console.log(standUp())
+console.log(Albus.debugsCode('Ron', 'React'));
 
 // PERSON INFO
 
 const Ginny = new Person({
   name: 'Ginny',
   age: 22,
-  location: 'Florida',
+  location: 'Florida'
 });
 
 const Hermione = new Person({
@@ -152,5 +154,7 @@ const Flitwick = new ProjectManager({
   favInstructor: 'Lupin',
 });
 
-
 // CONSOLE LOGS
+
+console.log(Ginny.speak())
+console.log(Hermione.speak())
